dae851a9dbfcc40cdc842cdea20f26875b540ea7
“PrabeshBimali” <prabeshlord111@gmail.com>
2022-01-26 14:35:02 +0545

    finished for submission

--- baghchal/minimax.py
+++ baghchal/minimax.py
@@ -8,17 +8,30 @@ class Ai:
     def __init__(self, depth=5):
         self.depth = depth
 
+
+    # static evaluation  of current state of the game
+    # param => board
+    # Returns => Int
+
     def static_evaluation(self, board):
         end = board.is_game_over()
         if not end:
+            # Maximizes for goat 
             return 1.5 * board.trapped_tigers - 1.2 * board.killed_goats
+
         winner = board.get_winner()
+
         if winner == "goat":
             return INF
         elif winner == "bagh":
             return -INF
         else:
             return 0
+
+    #
+    # Minimax Algorithm
+    # returns best move
+    #
 
     def minimax(self, board, depth=0, alpha=-INF, beta=INF, maxPlayer=True):
         if depth == 0 or board.is_game_over():
@@ -28,39 +41,65 @@ class Ai:
             maxEval = -INF*10
             best_move = 0
             for move in board.generate_moves_for_all_pieces("goat"):
-                b_copy = deepcopy(board)
-                b_copy.move(move)
-                eval_ = self.minimax(b_copy, depth - 1, alpha, beta, False)[1]
+
+                #
+                # Deep Copy of the board
+                #
+
+                tmp_board = deepcopy(board)
+                tmp_board.move(move)
+
+                eval_ = self.minimax(tmp_board, depth - 1, alpha, beta, False)[1]
+
                 if eval_ > maxEval:
                     maxEval = eval_
                     best_move = move
+
                 alpha = max(alpha, eval_)
+
                 if beta <= alpha:
                     break
             return best_move, maxEval
+
         else:
+
             minEval = INF*10
             best_move = 0
             
             for move in board.generate_moves_for_all_pieces("bagh"):
-                b_copy = deepcopy(board)
-                b_copy.move(move)
-                eval_ = self.minimax(b_copy, depth - 1, alpha, beta, True)[1]
+
+                #
+                # Deep copy of the board
+                #
+
+                tmp_board = deepcopy(board)
+                tmp_board.move(move)
+
+                eval_ = self.minimax(tmp_board, depth - 1, alpha, beta, True)[1]
+
                 if eval_ < minEval:
                     minEval = eval_
                     best_move = move
+
                 beta = min(beta, eval_)
+
                 if beta <= alpha:
                     break
             return best_move, minEval
+
 
     def _best_bagh_move(self, board):
         assert not board.is_game_over()
         return self.minimax(board, self.depth, maxPlayer=False)
 
+
     def _best_goat_move(self, board):
         assert not board.is_game_over()
         return self.minimax(board, self.depth, maxPlayer=True)
+
+        #
+        # Returns best move for the piece
+        #
 
     def get_best_move(self, board, turn):
         board = SimpleBoard(board.board, board.killed_goats, board.unused_goats, board.trapped_tigers)
